bf5aa9f0ebd81011f03a5bf11af06b53f1176e4a
Vijay <axelblaze14022000@gmail.com>
2023-07-24 23:08:23 +0530

    fixed whitespace issue

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const AboutPage = () => {
   return (
-    <div className="h-screen flex flex-col justify-between gap-4 bg-white dark:bg-gray-700 pt-20 py-2 px-4">
+    <div className="h-full lg:h-screen flex flex-col justify-between gap-4 bg-white dark:bg-gray-700 pt-20 py-2 px-4">
       <div className="max-w-3xl mx-auto">
         <h1 className="text-3xl font-bold mb-2 dark:text-yellow-50">
           About TextUtils
